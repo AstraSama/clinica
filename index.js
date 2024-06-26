87580439932b6097bd7c78313849d1b015e3b75d
Felipe <fe_lipe-mar_tins@hotmail.com>
2024-06-26 09:09:41 -0300

    Gerar id aleatório

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let consulta = {
 let passo = 0;
 let aux = "";
 let saida = 0;
+let id_aux = 0;
 
 process.stdout.write("\nDigite 'add' para adicionar nova consulta.\n'list' para listar as consultas.\n'update' para atualizar uma coluna.\n'rm' para remover uma consulta.\n'sair' para fechar o programa.\nResposta: ");
 
@@ -78,8 +79,40 @@ process.stdin.on('data', function(data) {
         case 4:
             consulta.data = Number(resposta);
 
-            process.stdout.write("Qual o id dessa consulta: ");
-            passo = 7;
+            process.stdout.write("Gerando o id dessa consulta: ");
+            id_aux = Math.floor((Math.random() * 10000) + 1);
+            
+            do {
+                if(lista_consultas != 0) {
+                    for(let i = 0; i < lista_consultas.length; i++) {
+                        if(lista_consultas[i].id == id_aux) {
+                            saida = 0;
+                        } else {
+                            saida = 1;
+        
+                        }
+                    }
+                } else {
+                    saida = 1;
+                    
+                }
+            } while (!saida);
+
+            if(saida) {
+                consulta.id = id_aux;
+                lista_consultas.push(consulta);
+                consulta = {
+                    nome: "",
+                    nome_medico: "",
+                    horario: "",
+                    data: ""
+                };
+    
+                process.stdout.write("\nDigite 'add' para adicionar nova consulta.\n'list' para listar as consultas.\n'update' para atualizar uma coluna.\n'rm' para remover uma consulta.\n'sair' para fechar o programa.\nResposta: ");
+                saida = 0;
+                passo = 0;
+
+            }
 
         break;
 
@@ -133,23 +166,6 @@ process.stdin.on('data', function(data) {
 
 
         break;
-
-        case 7:
-            consulta.id = Number(resposta);
-
-            lista_consultas.push(consulta);
-            consulta = {
-                nome: "",
-                nome_medico: "",
-                horario: "",
-                data: ""
-            };
-
-            process.stdout.write("\nDigite 'add' para adicionar nova consulta.\n'list' para listar as consultas.\n'update' para atualizar uma coluna.\n'rm' para remover uma consulta.\n'sair' para fechar o programa.\nResposta: ");
-            passo = 0;
-
-        break;
-
     }
     if(passo == 8) {
         process.exit();
